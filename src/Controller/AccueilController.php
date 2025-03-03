257ebf93d5bc6fbd8cdc33bf5291f0a5e66f7e7f
Your Name <you@example.com>
2025-03-03 15:17:42 +0100

    envoie calculatrice sans commentaire inutil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -19,7 +19,7 @@ class AccueilController {
 //todo
     #[Route(path: "/calculatrice/{nbr1}/{nbr2}/{operateur}", name:"app_accueil_calculatrice")]
     public function calculatrice(int $nbr1, int $nbr2, string $operateur):Response { 
-       if(is_numeric($nbr1) == true && is_numeric($nbr2) == true ){
+       if(is_numeric($nbr1) && is_numeric($nbr2) ){
             switch ($operateur){
                 case  "add": 
                     return new Response("L’addition de ". $nbr1 . " et de " . $nbr2 . " est égale au résultat : " . $nbr1 + $nbr2);
@@ -47,31 +47,3 @@ class AccueilController {
 
 
 
-
-/* #[Route(path: "/calculatrice/{nbr1}/{nbr2}/{operateur}", name:"app_accueil_calculatrice")]
-public function calculatrice(mixed $nbr1, mixed $nbr2, string $operateur):Response { //si int ça se lance pas
-    if(gettype($nbr1) == "integer" && gettype($nbr2) == "integer"){
-       
-        switch ($operateur){
-            case  "add": 
-                return new Response("L’addition de ". $nbr1 . " et de " . $nbr2 . " est égale au résultat : " . $nbr1 + $nbr2);
-            case  "sous": 
-                return new Response("La soustraction de ". $nbr1 . " et de " . $nbr2 . " est égale au résultat : " . $nbr1 - $nbr2);
-            case "multi":
-                return new Response("La multiplication de ". $nbr1 . " et de " . $nbr2 . " est égale au résultat : " . $nbr1 * $nbr2);
-            case "div":
-                if($nbr2 !=0){
-                    return new Response("La division de ". $nbr1 . " et de " . $nbr2 . " est égale au résultat : " . $nbr1 / $nbr2);
-                } /* else {
-                    return new Response("le deuxieme nombre ne doit pas tere Zero");
-                } //
-            default:
-                return new Response("L'opérateur n'est pas correct");
-    
-           }
-    } else {
-        return new Response(gettype($nbr1),  gettype($nbr2));
-       // return new Response("Veuillez rentrer des nombres please");
-    }
-} */
-  
