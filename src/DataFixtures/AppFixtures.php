270b0b16b4f88becaca072a1a91b882839e16854
Your Name <you@example.com>
2025-03-05 14:01:52 +0100

    remplissage de la bdd avec Faker Php et ajout de Category et article_category

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Account;
 use App\Entity\Article;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
@@ -13,23 +14,32 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Faker\Factory::create('fr_FR');
-        $tabAuthor = array();
+        $tabAuthor = [];
         for ($i = 0; $i < 50; $i ++) {
             $account = new Account();
             $account->setFirstname($faker->firstname())
                     ->setLastname($faker->lastname())
-                    ->setEmail($faker->email())
+                    ->setEmail($faker->unique()->email())
                     ->setPassword($faker->password())
                     ->setRoles('ROLE_USER');
             $manager->persist($account);
             $tabAuthor[] = $account;
         }
+        $tabCat = [];
+        for ($i = 0; $i < 30; $i ++) {
+            $cat = new Category();
+            $cat->setName($faker->word());
+            $manager->persist($cat);
+            $tabCat[] = $cat;
+        }
+
         for ($i = 0; $i < 100; $i ++){
             $article = new Article();
-            $article->setTitle($faker->sentence(5))
-                    ->setcontent($faker->text(300))
+            $article->setTitle($faker->sentence(3))
+                    ->setcontent($faker->realText(300, 2))
                     ->setCreateAt(new \DateTimeImmutable($faker->date()))
-                    ->setAuthor($faker->randomElement($tabAuthor));
+                    ->setAuthor($faker->randomElement($tabAuthor))
+                    ->addCategory($faker->randomElement($tabCat));
             $manager->persist($article);
         }
         $manager->flush();
